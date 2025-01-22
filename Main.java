07da04639895c5803152cae730c7cc8c2fa4e74d
Раиль <97154123+Abdulgazin@users.noreply.github.com>
2025-01-22 18:07:58 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,33 @@
 package com.walking.lessonEleventh.counterAggregation;
 
+import static com.walking.lessonEleventh.counterAggregation.CounterService.*;
+
 public class Main {
     public static void main(String[] args) {
-        Counter gasCounter=new Counter("счетчик газа",0,"кубометров");
-        Counter coldWaterCounter=new Counter("счетчик холодной воды",0,"кубометров");
-        Counter hotWaterCounter=new Counter("счетчик горячей воды",0,"кубометров");
-        Counter electricityCounter=new Counter("счетчик электричества",0,"киловатт");
-        Object[]Counters=new Object[4];
-        Counters[0]=gasCounter;
-        Counters[1]=coldWaterCounter;
-        Counters[2]=hotWaterCounter;
-        Counters[3]=electricityCounter;
-        CounterService counterService=new CounterService(Counters);
+        Counter gasCounter = new Counter("газ", 0, "кубометров");
+        Counter coldWaterCounter = new Counter("холодная вода", 0, "кубометров");
+        Counter hotWaterCounter = new Counter("горячая вода", 0, "кубометров");
+        Counter electricityCounter = new Counter("электричество", 0, "киловатт");
+        Counter[] Counters = new Counter[4];
+        Counters[0] = gasCounter;
+        Counters[1] = coldWaterCounter;
+        Counters[2] = hotWaterCounter;
+        Counters[3] = electricityCounter;
+        CounterService counterService = new CounterService(Counters);
+        //Counters[0].setCounterValue(25);
+        //gasCounter.increment();
+        //counterService.increaseValue(25);
+        //coldWaterCounter.increment();
+        Counters[1].increaseValue(35);
+        //hotWaterCounter.increaseValue(85);
+        //electricityCounter.increaseValue(123);
+        //gasCounter.resetValue();
+        printArray();
+    }
+
+    private static void printArray() {
+        for (Counter i : Counters) {
+            System.out.println(i);
+        }
     }
 }
